882f65aa83fcf9b17d37416957a9a99f7aac930f
ChinSeok Lee <allieuslee@gmail.com>
2012-10-25 21:50:16 +0900

    commit by eb & allieus & zookd

---
+++ 2012_10_25/eb_and_allieus_and_zookd/spec/oil_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+
+def price(galon)
+    rules = [[20, 0.9], [40, 0.8], [0, 0.75]]
+
+    rules.map do |period, price|
+    	quantity = [period, galon].min
+    	quantity = galon if period == 0
+    	galon -= quantity
+    	quantity * price
+    end.inject(&:+)
+end
+
+def notused_function
+=begin
+    while galon > 0
+    	quantity = [period[i], galon].min
+    	quantity = galon if period[i] == 0
+    	sum += quantity * price[i]
+    	galon -= quantity
+    	i += 1
+    end
+    return sum
+=end
+
+=begin
+    if galon <= 20
+        return galon * 90
+    elsif galon <= 60
+        return 1800 + (galon-20) * 80
+    else
+        return 1800 + 3200 + (galon-60) * 75
+    end
+=end
+
+end
+
+describe "#price" do
+    it "should calculate price" do
+        price(0).should == 0
+        price(10).should == 9
+        price(20).should == 18
+        price(21).should == 18.8
+        price(60).should == 50
+        price(61).should == 50.75
+        price(100).should == 80
+        price(200).should == 155
+    end
+end
