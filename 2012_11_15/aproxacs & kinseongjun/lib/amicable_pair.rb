250f40eb52d67ea15b2299241b43f76bc7223505
Chang Shin <aproxacs@gmail.com>
2012-11-16 03:33:33 +0900

    refactoring

--- 2012_11_15/aproxacs & kinseongjun/lib/amicable_pair.rb
+++ 2012_11_15/aproxacs & kinseongjun/lib/amicable_pair.rb
@@ -1,41 +1,47 @@
+class Fixnum
+  def real_submultiples
+    max = Math.sqrt(self).to_i
+    return [1] if max < 2
+
+    # (2..max).map {|i| self % i == 0 ? [i, self/i] : nil}.compact.flatten.push(1).sort.uniq    
+    # (2..max).inject([]) { |r,i| self % i == 0 ? r.concat([i, self/i]) : r}.uniq.push(1)
+
+    # (2..max).inject([]) do |ret, i| 
+    #   if self % i == 0
+    #     j = self/i
+    #     if i == j
+    #       ret.push(i)
+    #     else
+    #       ret.concat([i, j])
+    #     end
+    #   else
+    #     ret
+    #   end
+    # end.unshift(1)
+
+    (2..max).inject([]) {|r,i| self%i == 0 ? (j=self/i; i==j ? r.push(i) : r.concat([i,j])) : r}.unshift(1)
+  end
+end
+
 class AmicablePair
   def initialize(number=10)
     @number = number
   end
 
-  def submultiples(number)
-    max = Math.sqrt(number).ceil
-    (2..max).map {|i| number % i == 0 ? [i, number/i] : nil}.compact.flatten.push(1).sort.uniq
+  def sum_table
+    @sum_table ||= make_pairs
   end
 
-  def submultiples_old(number)
-      submultiples = (1...number).inject([]) do |a, i|
-      if number % i == 0
-        a << i
-      end
-      a
-    end 
-    submultiples  
+  def result
+    sum_table.map {|k,v| (k != v and sum_table[v] == k) ? [k,v].sort : nil}.compact.uniq
   end
 
-  def all_pairs
-    @all_pairs ||= make_pairs
-  end
-
+  private
   def make_pairs
-    (2..@number).inject({}) do |ret, i|
-      ret[i] = submultiples(i).inject(&:+)
+    (2..@number).inject({1 => -1}) do |ret, i|
+      ret[i] = i.real_submultiples.inject(&:+)
       ret
     end
   end
 
-  def result
-    # ret = []
-    all_pairs.map {|k,v| all_pairs[k] == v ? [k,v].sort : nil}.compact.uniq
-    # all_pairs.each do |k,v|
-    #   ret << [k,v].sort if all_pairs[k] == v
-    # end
-
-    # ret.uniq
-  end
 end
--- 2012_11_15/aproxacs & kinseongjun/spec/amicable_pair_spec.rb
+++ 2012_11_15/aproxacs & kinseongjun/spec/amicable_pair_spec.rb
@@ -1,49 +1,55 @@
 require "spec_helper"
-
+require "benchmark"
 
 require "amicable_pair"
+
 describe AmicablePair do
   let(:number) { 1000 }  
   let(:pair) { AmicablePair.new(number) }
-  describe "#initialize" do
+
+  describe "#sum_table" do
     
-    it "is initialized with number" do
-      AmicablePair.new(number)
+    it "sum table test" do
+      pair.sum_table[2].should == 1
+      pair.sum_table[3].should == 1
+      pair.sum_table[4].should == 3
+      pair.sum_table[220].should == 284
+      pair.sum_table[284].should == 220
     end
   end
 
-  describe "#submultiples" do
-
-    context "when target number is 220" do
-      let(:target) { 220 }
-      it "is [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]" do
-        pair.submultiples(target).should == [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]  
-      end
-    end
-
-    context "when target number is 284" do
-      let(:target) { 284 }
-      it "is [1, 2, 4, 71, 142]" do
-        pair.submultiples(target).should == [1, 2, 4, 71, 142]  
-      end
-    end
-  end
-
-  describe "#all_pairs" do
-    
-    it "does something" do
-      pair.all_pairs[220].should == 284
-      pair.all_pairs[284].should == 220
-    end
-  end
-
-  describe "#result", focus: true do
+  describe "#result" do
     let(:number) {10000}
-    it "does something" do
-      pair.result.should include([220, 284])
-      pair.result.should_not include([284, 220])
+    it "armicable pair test" do
+      AmicablePair.new(10).result.should == []
+      AmicablePair.new(1000).result.should == [[220, 284]]
+      AmicablePair.new(10000).result.should == [[220, 284], [1184, 1210], [2620, 2924], [5020, 5564], [6232, 6368]]
+    end
+
+    it "benchmark" do
+      p Benchmark.measure { AmicablePair.new(20000).result }
     end
   end
 
 end
 
+
+describe Fixnum do
+  describe "#real_submultiples" do
+    it "real_submultiples test" do
+      284.real_submultiples.sort.should == [1, 2, 4, 71, 142]
+      220.real_submultiples.sort.should == [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]
+      10.real_submultiples.sort.should == [1,2,5]
+      8.real_submultiples.sort.should == [1,2,4]
+      6.real_submultiples.sort.should == [1,2,3]
+      4.real_submultiples.sort.should == [1,2]
+      3.real_submultiples.sort.should == [1]
+      2.real_submultiples.sort.should == [1]
+      1.real_submultiples.sort.should == [1]
+    end
+
+    it "benchmark" do
+      p Benchmark.measure { 1000.times { 220.real_submultiples } }
+    end
+  end
+end
